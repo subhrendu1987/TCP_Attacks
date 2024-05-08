b2db53308c3f594a92d245cab3105d29f3e34534
subhrendu1987 <subhrendu@idrbt.ac.in>
2024-05-08 12:23:49 +0530

    modified:   WebResponseMeasure.sh

--- WebResponseMeasure.sh
+++ WebResponseMeasure.sh
@@ -9,4 +9,4 @@ curl -w 'Return code: %{http_code}; Bytes Received: %{size_download};
  ----------\n
  time_total:  %{time_total}s\n
 Response Time: %{time_total}\n' http://172.20.0.2:80 -m 2 -o /dev/null -s; 
-done|tee /dev/tty|awk '{ sum+= $NF; n++ } END { if (n > 0) print "Average Response time =",sum / n; }'
+done|tee /dev/tty|grep "Response Time:" |awk '{ sum+= $NF; n++ } END { if (n > 0) print "Average Response time =",sum / n; }'
